b5dd062ebbc6fe82fb729452c2472172aace7236
Vizago <zaebiyt@gmail.com>
2023-05-25 14:49:00 +0700

    Reading NODE_ENV update

--- master/src/server_entry_point.js
+++ master/src/server_entry_point.js
@@ -1,7 +1,11 @@
 const Hapi = require("@hapi/hapi");
 const routes = require("./server_routes");
+const process = require("process");
+
+console.log(process.env.NODE_ENV);
 
 const init = async () => {
+
     const server = Hapi.server({
         host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
         port: 5000,
